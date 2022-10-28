01be1c4bd32c67df90e0f8836956cd3adc51c27d
Luis Enrique Ramirez <luis_enrique_ramirez@claro.com.do>
2022-10-28 17:53:26 -0400

    creating uiState holder

---
+++ app/src/main/java/com/erapps/moviesinfoapp/ui/shared/UiState.kt
@@ -0,0 +1,11 @@
+package com.erapps.moviesinfoapp.ui.shared
+
+sealed interface UiState {
+    data class Success<out T>(val data: T) : UiState
+    data class Error(
+        val errorMessage: String? = null, val errorStringResource: Int? = null
+    ) : UiState
+
+    object Loading : UiState
+    object Empty : UiState
+}
